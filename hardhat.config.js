095e59b5ac8060fe41cf38da254839789c8f6f04
0xWhale <100045279+0xWhale@users.noreply.github.com>
2025-05-09 18:40:05 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,2 +1,1 @@
-PK= 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-Mnemonic = beyond voyage ankle better stay maid narrow donor chuckle girl accident arrange science visit trade muscle tortoise ginger
+clean
